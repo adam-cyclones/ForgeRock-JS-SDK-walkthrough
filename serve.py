36f9fba763850acabbc54d62f412b33e14b099fc
Adam Crockett <adam.crockett@forgerock.com>
2021-03-11 16:30:15 +0000

    task.1.step.1.2 DNS alias within  for https cert

--- serve.py
+++ serve.py
@@ -1,1 +1,10 @@
-python3 -m http.server
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+httpd = HTTPServer(('localhost', 8088), SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket (httpd.socket,
+        keyfile="./certs/adam-sdk.app+1-key.pem",
+        certfile='./certs/adam-sdk.app+1.pem', server_side=True)
+
+httpd.serve_forever()
