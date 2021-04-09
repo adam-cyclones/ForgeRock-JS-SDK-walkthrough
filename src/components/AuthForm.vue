e235af9de8b1b6e0073ae66c9fe902c28a980cfe
Adam Crockett <adam.crockett@forgerock.com>
2021-04-09 18:10:40 +0100

    task.2.step.1.1 form as-is before I make it dynamic, I have hooked it up so that AppRoot handles logic as it also has the plugin and control over conditional rendering

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,13 +1,13 @@
 <template>
-    <form id="form" novalidate>
+    <form @submit.prevent="handleSubmit" ref="form" novalidate>
         <fieldset>
             <legend>Login</legend>
             <div>
-                <label for="username">*Username</label>
+                <label for="username">* Username</label>
                 <input required id="username" type="text" />
             </div>
             <div>
-                <label for="password">*Password</label>
+                <label for="password">* Password</label>
                 <input required id="password" type="password" />
             </div>
             <hr />
@@ -18,6 +18,14 @@
 
 <script>
 export default {
-    name: 'AuthForm'
+    name: 'AuthForm',
+    methods: {
+        handleSubmit() {
+            const { form } = this.$refs;
+            if (form.checkValidity()) {
+                this.$emit('submit');
+            }
+        }
+    }
 }
 </script>
