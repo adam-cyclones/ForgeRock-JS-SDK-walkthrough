f889d7acaaa94c3e22d1e0f806cabe8f170f382f
Adam Crockett <adam.crockett@forgerock.com>
2021-04-20 16:29:42 +0100

    task.2.step.5.6 components which represent the callbacks we are handling

---
+++ src/components/callbacks/NameCallback.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <label :for='id'><slot></slot></label>
+        <input required :id='id' type="text"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UsernameCallback',
+    props: {
+        id: String
+    }
+}
+</script>
---
+++ src/components/callbacks/PasswordCallback.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <label :for='id'><slot></slot></label>
+        <input required :id='id' type="password"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PasswordCallback',
+    props: { 
+        id: String
+    }
+}
+</script>
