9c459f61c619dcf9c5e19f6a98962b24e6bce3fc
Adam Crockett <adam.crockett@forgerock.com>
2021-04-20 16:25:14 +0100

    task.2.step.5.6

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,17 +1,16 @@
 <template>
-    <form @submit.prevent="handleSubmit" ref="form" novalidate>
+    <form @submit.prevent="handleSubmit" ref="form" @change='simpleValidate' novalidate>
         <fieldset>
             <legend>Login</legend>
-            <div>
-                <label for="username">* Username</label>
-                <input required id="username" type="text" />
-            </div>
-            <div>
-                <label for="password">* Password</label>
-                <input required id="password" type="password" />
-            </div>
+            <component
+                :key="callback.payload._id"
+                v-for='callback in frTreeUIData'
+                :is="callback.component"
+                :id="formatValidID(callback.payload._id)">
+                {{callback.payload.output[0].value}}
+            </component>
             <hr />
-            <button type="submit">Authenticate</button>
+            <button :disabled='!valid' @click='handleNext' type="button">Next</button>
         </fieldset>
     </form>
 </template>
@@ -19,12 +18,32 @@
 <script>
 export default {
     name: 'AuthForm',
+    props: {
+        frTreeUIData: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            valid: false
+        }
+    },
     methods: {
         handleSubmit() {
             const { form } = this.$refs;
             if (form.checkValidity()) {
                 this.$emit('submit');
             }
+        },
+        formatValidID(id) {
+            return `callback-${id}`;
+        },
+        handleNext() {
+            this.$emit('next');
+        },
+        simpleValidate() {
+            this.valid = this.$refs.form.checkValidity();
         }
     }
 }
